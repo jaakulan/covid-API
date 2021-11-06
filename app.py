47eec1069323fbf30e7d00082a03eff5c96ad62b
Jaakulan Subeethakumar <jaakulans@hotmail.com>
2021-11-06 00:52:31 -0400

    completed adding csvs to daily tables

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import csv
 import pandas as pd
+from dailyReportTools import dailyCSV as nd
 
 app = Flask(__name__)
 
@@ -9,7 +10,7 @@ app.config['UPLOAD_FOLDER'] =  UPLOAD_FOLDER
 
 @app.route('/')
 def index():
-    return "hello"
+    return "hello" 
 
 #this will overwrite all databases and create a new database for daily csv file
 @app.route('/daily/addNewCSV', methods=['POST'])
@@ -17,8 +18,21 @@ def postCSV():
     if request.method == 'POST':
         file = request.files['file']
         data = pd.read_csv(file, header = [0])
-        print(data.to_dict('records'))
+        dataDic = data.to_dict('records')
+        nd.newCSV(dataDic)
     return "sucess \n" + data.to_string()
+
+@app.route('/daily/viewAll', methods=['GET'])
+def view():
+    if request.method == 'GET':
+        nd.viewData()
+    return "success"
+
+@app.route('/daily/deleteAll', methods=['DELETE'])
+def deleteAll():
+    if request.method == 'DELETE':
+        nd.deleteAllData()
+    return "success"
 
 if __name__ == '__main__':
     app.debug =True
---
+++ dailyReportTools/dailyCSV.py
@@ -0,0 +1,136 @@
+from typing import List, Optional
+import psycopg2 as pg
+from psycopg2 import Error
+import psycopg2.extras
+
+
+class dbConnection:
+    def __init__(self) -> None:
+        """Initialize this Recommender, with no database connection yet.
+        """
+        self.db_conn = None
+
+    def startConnection(self, user: str, password: str, host: str, port: str, database: str):
+        try:
+            # Connect to an existing database
+            self.db_conn = pg.connect(user=user,
+                                        password=password,
+                                        host=host,
+                                        port=port,
+                                        database=database)
+            print("you are connected to database")
+            cur = self.db_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+            cur.execute(
+                """
+                    CREATE SCHEMA IF NOT EXISTS covidCases;
+                    CREATE TABLE IF NOT EXISTS dailyCases (
+                        combined TEXT NOT NULL,
+                        deaths INT NOT NULL,
+                        confirmed INT NOT NULL,
+                        active INT NOT NULL,
+                        recovered INT NOT NULL,
+                        d TIMESTAMP NOT NULL,
+                        PRIMARY KEY (combined)
+                    );
+                """
+                )
+            cur.close()
+            self.db_conn.commit()
+        except (Exception, Error) as error:
+            print("Error while connecting to PostgreSQL", error)
+
+    def disconnect_db(self):
+        try:
+            self.db_conn.close()
+            print("disconnected")
+        except pg.Error:
+            print("error disconnecting")
+
+    def restartDB(self) -> bool:
+        try:
+            cur = self.db_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+            cur.execute(
+                """
+                    DROP TABLE IF EXISTS dailyCases CASCADE;
+                    DROP SCHEMA IF EXISTS covidCases CASCADE;
+                    CREATE SCHEMA covidCases;
+                    SET SEARCH_PATH TO covidCases;
+                    CREATE TABLE dailyCases (
+                        combined TEXT NOT NULL,
+                        deaths INT NOT NULL,
+                        confirmed INT NOT NULL,
+                        active INT NOT NULL,
+                        recovered INT NOT NULL,
+                        d TIMESTAMP NOT NULL,
+                        PRIMARY KEY (combined)
+                    );
+                """
+                )
+            cur.close()
+            self.db_conn.commit()
+            return True
+        except pg.Error:
+            print(cur.statusmessage)
+            return False
+    
+    def insertNewData(self, data)->bool:
+        try:
+            cur = self.db_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+            for dataPoint in data:
+                cur.execute(
+                    """
+                    Insert into dailyCases VALUES
+                    (%s, %s, %s, %s, %s, %s);
+                    
+                    """,
+                    (dataPoint['Combined_Key'], dataPoint['Deaths'], dataPoint['Confirmed'], dataPoint['Active'], dataPoint['Recovered'], dataPoint['Last_Update'])
+                    )
+                print(cur.statusmessage)
+            cur.close()
+            self.db_conn.commit()
+            return True
+        except pg.Error:
+            print(cur.statusmessage)
+            return False
+    
+    def viewAllData(self):
+        try:
+            cur = self.db_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+            cur.execute(
+                    """
+                    select * from dailyCases
+                    
+                    """
+                    )
+            print(cur.statusmessage)
+            data = cur.fetchall()
+            cur.close()
+            self.db_conn.commit()
+            return data
+        except pg.Error:
+            print(cur.statusmessage)
+            return False
+        
+
+
+def getConnection():
+    covid = dbConnection()
+    covid.startConnection("fysyfysyrxbtwb", "c003474f786bc37b03b31d4f0377713a3a29becb8c9b111062db7ba496aa34e1", "ec2-3-228-134-188.compute-1.amazonaws.com", "5432", "dd5hvt80750lfu")
+    return covid
+
+def newCSV(data):
+    covid = getConnection()
+    print("DB has restarted", covid.restartDB())
+    covid.insertNewData(data)
+    covid.disconnect_db()
+        
+def viewData():
+    covid = getConnection()
+    print(covid.viewAllData())
+    covid.disconnect_db()
+
+def deleteAllData():
+    covid = getConnection()
+    print(covid.restartDB())
+    covid.disconnect_db()
+
