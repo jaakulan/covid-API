9daaa290409b28ef6247e8e5c6d5c61bbb541bec
Jaakulan Subeethakumar <jaakulans@hotmail.com>
2021-11-10 03:05:11 -0500

    query is finished
    now i need to update the database with date and with extra values

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify
 import csv
 import pandas as pd
-import datetime
 from dailyReportTools import dailyCSV as nd
+from cleaningTools import dataCleaning as clean
 
 app = Flask(__name__)
 
@@ -38,81 +38,30 @@ def deleteAll():
 def getInfo():
     if request.method == 'GET':
         data = request.args.getlist('data')
-        data = cleanDataQuery(data)
         dateStart = request.args.getlist('dateStart')
         dateEnd = request.args.getlist('dateEnd')
         dateSpecific = request.args.getlist('date')
-        dates = cleanDateQuery(dateStart, dateEnd, dateSpecific)
+        #clean all the data that was requested through URL
+        countries = clean.locationQuery(request.args.getlist('country'))
+        #Region is for province or state
+        region = clean.locationQuery(request.args.getlist('region'))
+        key = clean.locationQuery(request.args.getlist('combined'))
+        data = clean.dataQuery(data)
+        dates = clean.dateQuery(dateStart, dateEnd, dateSpecific)
+        print(countries)
+        print(region)
+        print(key)
         print(data)
         print(dates)
-        return "success"
+        validityCheck = clean.infoValidity(data, dates, countries, region, key)
+        if validityCheck[0]:
+            print(nd.queryData(data, dates, countries, region, key))
+            return "success"
+        print(validityCheck[1])
+        return validityCheck[1]
     else:
         return "Incorrect HTTP Method!"
 
-def cleanDateQuery(dateStart, dateEnd, dateSpecific):
-    if dateStart == [] and dateEnd == []:
-        if len(dateSpecific) == 1:
-            #do cleansing of dateSpecific
-            cleanDates = checkDates(dateSpecific)
-            if(cleanDates[0]):
-                return [True, [dateSpecific]]
-            else:
-                return cleanDates[1] + " is an incorrect data format, should be YYYY-MM-DD"
-        elif len(dateSpecific) == 0:
-            # because no dates were specified
-            return [True, None] 
-        else:
-            return [False, "Only 1 specific date can be queried at a time!"]
-    elif dateSpecific == []:
-        #this means dateStart or dateEnd both have queries and we will clean the duration
-        if (len(dateStart) == 1 and len(dateEnd) == 1):
-            cleanDates = checkDates([dateStart[0], dateEnd[0]])
-            if(cleanDates[0]):
-                #since dates are in iso format we can do direct comparison
-                if cleanDates[1][0] < cleanDates[1][1]:
-                    return [True, [dateStart[0], dateEnd[0]]]
-                else:
-                    return [False, "The start date needs to have occured before entered the end date"]
-            else:
-                return cleanDates[1] + " is an incorrect data format, should be YYYY-MM-DD"
-        else:
-            return [False, "Incorrect amount of dates entered for dateStart/dateEnd, Or missing date for one of those parameters!"]
-    else:
-        return [False, "Only 1 specific date Or 1 duration of two dates can be queried at a time!"]
-
-def checkDates(dates):
-    for i in dates:
-        if not checkValidDate(i):
-            return [False, i]
-    return [True]
-
-def checkValidDate(date):
-    try:
-        datetime.datetime.strptime(date, '%Y-%m-%d')
-        return True
-    except:
-        return False # "Incorrect data format, should be YYYY-MM-DD"]
-
-def cleanDataQuery(data):
-    #get rid of repeated data queries
-    data = list(set(data))
-    length = len(data)
-    #return null if data contains illegal queries
-    legal = ['deaths', 'confirmed', 'active', 'recovered']
-    illegal = []
-    
-
-    for i in range(length):
-        data[i] = data[i].strip().lower()
-        if data[i] not in legal:
-            illegal.append(data[i])
-    
-
-    if len(illegal) > 0:
-        return [False, illegal]
-    elif len(data)<1:
-        return [False, None]
-    return [True, data]
 
 if __name__ == '__main__':
     app.debug =True
--- dailyReportTools/dailyCSV.py
+++ dailyReportTools/dailyCSV.py
@@ -23,7 +23,10 @@ class dbConnection:
             cur.execute(
                 """
                     CREATE SCHEMA IF NOT EXISTS covidCases;
+                    SET SEARCH_PATH TO covidCases;
                     CREATE TABLE IF NOT EXISTS dailyCases (
+                        region TEXT NOT NULL,
+                        country TEXT NOT NULL,
                         combined TEXT NOT NULL,
                         deaths INT NOT NULL,
                         confirmed INT NOT NULL,
@@ -54,6 +57,8 @@ class dbConnection:
                     SET SEARCH_PATH TO covidCases;
                     DROP TABLE IF EXISTS dailyCases CASCADE;    
                     CREATE TABLE dailyCases (
+                        region TEXT NOT NULL,
+                        country TEXT NOT NULL,
                         combined TEXT NOT NULL,
                         deaths INT NOT NULL,
                         confirmed INT NOT NULL,
@@ -75,13 +80,15 @@ class dbConnection:
         try:
             cur = self.db_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
             for dataPoint in data:
+                print(dataPoint['Province_State'], dataPoint['Country_Region'])
                 cur.execute(
                     """
+                    SET SEARCH_PATH TO covidCases;
                     Insert into dailyCases VALUES
-                    (%s, %s, %s, %s, %s, %s);
+                    (%s, %s, %s, %s, %s, %s, %s, %s);
                     
                     """,
-                    (dataPoint['Combined_Key'], dataPoint['Deaths'], dataPoint['Confirmed'], dataPoint['Active'], dataPoint['Recovered'], dataPoint['Last_Update'])
+                    (dataPoint['Province_State'], dataPoint['Country_Region'], dataPoint['Combined_Key'], dataPoint['Deaths'], dataPoint['Confirmed'], dataPoint['Active'], dataPoint['Recovered'], dataPoint['Last_Update'])
                     )
                 print(cur.statusmessage)
             self.db_conn.commit()
@@ -93,10 +100,12 @@ class dbConnection:
     
     def viewAllData(self):
         try:
+            combined = "d = d"
             cur = self.db_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
             cur.execute(
                     """
-                    select * from dailyCases
+                    SET SEARCH_PATH TO covidCases;
+                    select * from dailyCases where """ +combined+ """ ;
                     
                     """
                     )
@@ -109,9 +118,31 @@ class dbConnection:
             print(cur.statusmessage)
             return False
         
-    def queryByComKey(self, query):
-
-        return
+    def query(self, data, dates, countries, region, key):
+        try:
+            print(data, dates, countries, region, key)
+            cur = self.db_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+            
+            cur.execute(
+                    """
+                    SET SEARCH_PATH TO covidCases;
+                    select """ +data+ """ from dailyCases
+                    where (""" +key+ """ Or
+                    """ +countries+ """ Or
+                    """ +region+ """) and
+                    """ +dates+ """;
+                    """
+                    )
+            print(cur.statusmessage)
+            info = cur.fetchall()
+            cur.close()
+            self.db_conn.commit()
+            print("the info is ")
+            print(data, info)
+            return info
+        except pg.Error:
+            print(cur.statusmessage)
+            return False
 
         
 def getConnection():
@@ -136,3 +167,64 @@ def deleteAllData():
     print(covid.restartDB())
     covid.disconnect_db()
 
+def queryData(data, dates, countries, region, key):
+    covid = getConnection()
+    data = dataString(data[1])
+    print(dates)
+    dates = dateString(dates[1])
+    countries = countryString(countries[1])
+    region = regionString(region[1])
+    key= keyString(key[1])
+    data = covid.query(data, dates, countries, region, key)
+    covid.disconnect_db()
+
+#########################################################__helper functions__#######################################
+
+def dataString(data):
+    return ",".join(data)
+
+def dateString(dates):
+    #3 cases
+    length = len(dates)
+    print(dates)
+    if(length==2):                                                           #case 1: two dates
+        return "d >= '" +dates[0]+" 00:00:00'"+" AND d <= '"+dates[1]+" 23:59:59'"
+    elif(length==1):                                                         #case 2: one date
+        return "d >= '"+dates[0]+" 00:00:00'"+" AND d <= '"+dates[0]+" 23:59:59'"
+    else:                                                                    #case 3: no dates
+        return "d = d"
+
+def countryString(countries):
+    #if query has countries
+    if(len(countries)>= 1):
+        for i in range(len(countries)):
+            countries[i] = "'"+countries[i]+"'"
+        countryList = ",".join(countries)
+        return "country in ("+countryList+")"
+    #if query has no countries
+    else:
+        return "country = country"
+
+def regionString(regions):
+    #if query has regions
+    if(len(regions)>= 1):
+        for i in range(len(regions)):
+            regions[i] = "'"+regions[i]+"'"
+        regionList = ",".join(regions)
+        return "region in ("+regionList+")"
+    #if query has no regions
+    else:
+        return "region = region"
+
+def keyString(keys):
+    #if query has regions
+    if(len(keys)>= 1):
+        for i in range(len(keys)):
+            keys[i] = "'"+keys[i]+"'"
+        keyList = ",".join(keys)
+        return "combined in ("+keyList+")"
+    #if query has no regions
+    else:
+        return "combined = combined"
+
+    
