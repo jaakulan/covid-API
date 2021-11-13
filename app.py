e361803c470ace0acf159e4d35d14af607103067
Alexandra Thompson <jenniferalexandra.thompson@mail.utoronto.ca>
2021-11-13 18:14:07 -0500

    Finished queries + view, made query seperate string, fixed bugs, applied to deaths and recovered as well as confirmed.

--- app.py
+++ app.py
@@ -52,31 +52,68 @@ def deleteAllnd():
 @app.route('/timeSeries/addNewCSV', methods=['POST'])
 def addNewCSVts():
     if request.method == 'POST':
+        datatypels = request.args.getlist('type')
+
+        if len(datatypels) == 0:
+            return "No Data Type Specified in this query"
+        datatype = datatypels[0]
+
+
         file = request.files['file']
         data = pd.read_csv(file, header=[0])
         dataDic = data.to_dict('records')
-        ts.newCSV(dataDic)
+        ts.newCSV(dataDic, datatype)
     return "success"
 
 @app.route('/timeSeries/updateCSV', methods=['POST'])
 def updateCSVts():
     if request.method == 'POST':
+        print("here")
+        datatypels = request.args.getlist('type')
+        if len(datatypels) == 0:
+            return "No Data Type Specified in this query"
+        datatype = datatypels[0]
+
+        print(datatype)
+
         file = request.files['file']
         data = pd.read_csv(file, header=[0])
         dataDic = data.to_dict('records')
-        ts.addCSV(dataDic)
+        ts.addCSV(dataDic, datatype)
     return "success"
 
 @app.route('/timeSeries/viewAll', methods=['GET'])
 def viewts():
     if request.method == 'GET':
-        ts.viewData()
-    return "success"
+
+        datatypels = request.args.getlist('type')
+        if len(datatypels) == 0:
+            return "No Data Type Specified in this query"
+        datatype = datatypels[0]
+
+        filetypels = request.args.getlist('file')
+        if len(filetypels) == 0:
+            return "No File Type Specified in this query"
+        filetype = filetypels[0]
+
+        data = ts.viewData(datatype)
+        data = json.dumps(data)
+        if (filetype == 'json' or filetype == 'JSON'):
+            return data
+        else:
+            data = pd.read_json(data)
+            return data.to_csv(index=False).replace("to_char", "d")
+    return "failure"
 
 @app.route('/timeSeries/deleteAll', methods=['DELETE'])
 def deleteAllts():
     if request.method == 'DELETE':
-        ts.deleteAllData()
+        datatypels = request.args.getlist('type')
+        if len(datatypels) == 0:
+            return "No Data Type Specified in this query"
+        datatype = datatypels[0]
+
+        ts.deleteAllData(datatype)
     return "success"
 
 @app.route('/timeSeries/info', methods=['GET'])
@@ -85,14 +122,14 @@ def getInfots():
 
         #Version without cleaning
 
-        typerls = request.args.getlist('type')
+        datatypels = request.args.getlist('type')
         datels = request.args.getlist('date')
         countryls = request.args.getlist('country')
         regionls = request.args.getlist('region')
 
 
         # Ensure type exists
-        if len(typerls) == 0:
+        if len(datatypels) == 0:
             return "No Type Specified in this query"
 
         # Assign date accordingly
@@ -111,10 +148,8 @@ def getInfots():
                 combined = countryls[0]
                 combined = ''.join(c for c in combined if c.isalnum())
 
-
-
-
-        return jsonify(ts.query(typerls[0], date, combined))
+        return json.dumps(ts.query(date, combined,datatypels[0]))
+        #return jsonify(ts.query(typerls[0], date, combined))
 
 
     else:
